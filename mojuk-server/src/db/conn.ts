53fe146a181a7ca09ab96d1f0f8885a6e13ddf21
charmingjae <beingeskimo@gmail.com>
2022-09-17 23:53:04 +0900

    feat: check database connection

--- mojuk-server/src/db/conn.ts
+++ mojuk-server/src/db/conn.ts
@@ -1,14 +1,26 @@
 import mysql = require("mysql");
 import { dbProperty } from "./property";
 
-const doConnect = async () => {
+const handleConnection = () => {
   const connection = mysql.createConnection(dbProperty);
-  console.log(connection);
-  //   try {
-  //     await connection.connect();
-  //   } catch (e) {
-  //     console.log("Error");
-  //   }
+  connection.connect(function (err: any) {
+    if (err) {
+      console.log("Database connection error");
+      setTimeout(handleConnection, 2000);
+    }
+  });
+
+  connection.on("error", function (err: any) {
+    console.log("Connection Error Occured");
+    if (
+      err.code === "PROTOCOL_CONNECTION_LOST" ||
+      err.code === "PROTOCOL_PACKETS_OUT_OF_ORDER"
+    ) {
+      return handleConnection();
+    } else {
+      throw err;
+    }
+  });
 };
 
-export { doConnect };
+export { handleConnection };
--- mojuk-server/src/index.ts
+++ mojuk-server/src/index.ts
@@ -1,6 +1,6 @@
 import express = require("express");
 import dotenv = require("dotenv");
-import { doConnect } from "./db/conn";
+import { handleConnection } from "./db/conn";
 const app: express.Application = express();
 dotenv.config();
 
@@ -10,5 +10,5 @@ app.listen(process.env.SERVER_PORT, () => {
   console.log(
     "Exress server listen... in port < " + process.env.SERVER_PORT + " >"
   );
-  doConnect();
+  handleConnection();
 });
