9097703a568b295d741b9a8dc7feeb3d176c69e2
charmingjae <beingeskimo@gmail.com>
2022-09-27 23:43:10 +0900

    feat: Toast UI editor

--- mojuk-client/src/page/Write/component.tsx
+++ mojuk-client/src/page/Write/component.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router";
 import BoardSelectBox from "../../component/BoardSelectBox";
 import Input from "../../component/Input";
 import styles from "./styles.module.css";
+import { Editor } from "@toast-ui/react-editor";
+import "@toast-ui/editor/dist/toastui-editor.css";
 
 const WriteContentsHeader = ({ ...props }: any) => {
   const { boardType } = useParams();
@@ -35,7 +37,24 @@ const WriteContentsHeader = ({ ...props }: any) => {
 };
 
 const WriteContentsBody = ({ ...props }: any) => {
-  return <div className={styles.write_contents_body}>body</div>;
+  return (
+    <div className={styles.write_contents_body}>
+      <Editor
+        placeholder="내용을 입력해주세요."
+        previewStyle="vertical" // 미리보기 스타일 지정
+        height="90%" // 에디터 창 높이
+        initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
+        toolbarItems={[
+          // 툴바 옵션 설정
+          ["heading", "bold", "italic", "strike"],
+          ["hr", "quote"],
+          ["ul", "ol", "task", "indent", "outdent"],
+          ["table", "image", "link"],
+          ["code", "codeblock"],
+        ]}
+      ></Editor>
+    </div>
+  );
 };
 
 const WriteContentsWrapper = ({ ...props }: any) => {
--- mojuk-client/src/root/Canvas/index.tsx
+++ mojuk-client/src/root/Canvas/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./styles.module.css";
 import Component from "./component";
 
 const Canvas = () => {
